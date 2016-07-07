bfeb6de0720e06c39e291fac8ff8e041dd76bb30
Hunter Stevens <onebree@gmail.com>
2016-07-07 10:10:37 -0400

    Add components for item grid

--- components/app.jsx
+++ components/app.jsx
@@ -2,6 +2,44 @@ import React from 'react'
 import ReactDOM from "react-dom"
 import { render } from 'react-dom'
 
+// ITEMS FOR SALE
+
+var Item = React.createClass({
+  render: function() {
+    return (
+      <div className="col-md-4">
+        <div className="thumbnail">
+          <img src="http://placehold.it/400x300" className="img-responsive" />
+          <div className="caption">
+            <big>{this.props.item.name}</big>
+            <p>{this.props.item.price}</p>
+            <p><button className="btn btn-primary">Add to cart</button></p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
+
+var ItemGrid = React.createClass({
+  render: function() {
+    var itemGrid = [];
+    this.props.items.forEach(function(item) {
+      itemGrid.push(<Item item={item} key={item.id} />);
+    });
+    return (
+      <div className="itemGrid">
+        <h2>Items For Sale</h2>
+        <div className="row">
+          {itemGrid}
+        </div>
+      </div>
+    );
+  }
+});
+
+// SHOPPING CART
+
 var ItemRow = React.createClass({
   render: function() {
     return (
@@ -61,10 +99,18 @@ var ShoppingCart = React.createClass({
 var ITEMS = [
   { id: 1, name: "Nexus 6P", price: "$499.99" },
   { id: 2, name: "OnePlus 3", price: "$399.99" },
-  { id: 3, name: "Kindle Paperwhite", price: "$119.99" }
+  { id: 3, name: "Kindle Paperwhite", price: "$119.99" },
+  { id: 4, name: "Reversible Down Comforter", price: "$45.99" },
+  { id: 5, name: "Women's Bag Gloves", price: "$39.55" },
+  { id: 6, name: "Casio Digital Watch", price: "$14.99" }
 ]
 
 ReactDOM.render(
+  <ItemGrid items={ITEMS} />,
+  document.getElementById("itemGrid")
+);
+
+ReactDOM.render(
   <ShoppingCart items={ITEMS} />,
-  document.getElementById("react")
+  document.getElementById("shoppingCart")
 );
