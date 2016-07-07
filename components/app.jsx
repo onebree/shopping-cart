eb5fa7738525e75c28b41b0994789fcd1b1d9ef4
Hunter Stevens <onebree@gmail.com>
2016-07-07 11:01:59 -0400

    Add states, outbound action

--- components/app.jsx
+++ components/app.jsx
@@ -5,6 +5,17 @@ import { render } from 'react-dom'
 // ITEMS FOR SALE
 
 var Item = React.createClass({
+  getInitialState: function(){
+    return { quantity: 0 };
+  },
+
+  addToCart: function(e) {
+    var q = this.state.quantity;
+    q += 1;
+    this.setState({ quantity: q });
+    console.log(this.props.item.name + " ADDED - " + q);
+  },
+
   render: function() {
     return (
       <div className="col-md-4">
@@ -12,8 +23,8 @@ var Item = React.createClass({
           <img src="http://placehold.it/400x300" className="img-responsive" />
           <div className="caption">
             <big>{this.props.item.name}</big>
-            <p>{this.props.item.price}</p>
-            <p><button className="btn btn-primary">Add to cart</button></p>
+            <p>${this.props.item.price}</p>
+            <p><button className="btn btn-primary" onClick={this.addToCart}>Add to cart</button></p>
           </div>
         </div>
       </div>
@@ -45,40 +56,36 @@ var ItemRow = React.createClass({
     return (
       <tr>
         <td>{this.props.item.name}</td>
-        <td>{this.props.item.price}</td>
+        <td>${this.props.item.price}</td>
       </tr>
     );
   }
 });
 
 var ItemTable = React.createClass({
+  addItem: function(e, item) {
+    // TODO - submit to server, refresh list
+    // grab Items with q > 1, add to items arr, increase subtotal (+= item.quantity * item.price)
+  },
+
+  getInitialState: function() {
+    return { items: [], subtotal: 0 };
+  },
+
   render: function() {
     var rows = [];
-    this.props.items.forEach(function(item) {
+    this.state.items.forEach(function(item) {
       rows.push(<ItemRow item={item} key={item.id} />);
     });
     return (
       <table className="table">
         <thead>
-          <tr>
-            <th>Name</th>
-            <th>Price</th>
+          <tr className="success">
+            <th>Items: {this.state.items.length}</th>
+            <th>Subtotal: ${this.state.subtotal}</th>
           </tr>
         </thead>
         <tbody>{rows}</tbody>
-      </table>
-    );
-  }
-});
-
-var TotalRow = React.createClass({
-  render: function() {
-    return (
-      <table className="table">
-        <tr className="success">
-          <th>Items: 0</th>
-          <th>Subtotal: $0.00</th>
-        </tr>
       </table>
     );
   }
@@ -89,20 +96,19 @@ var ShoppingCart = React.createClass({
     return (
       <div className="shoppingCart">
         <h2>Shopping Cart</h2>
-        <TotalRow />
-        <ItemTable items={this.props.items} />
+        <ItemTable />
       </div>
     );
   }
 });
 
 var ITEMS = [
-  { id: 1, name: "Nexus 6P", price: "$499.99" },
-  { id: 2, name: "OnePlus 3", price: "$399.99" },
-  { id: 3, name: "Kindle Paperwhite", price: "$119.99" },
-  { id: 4, name: "Reversible Down Comforter", price: "$45.99" },
-  { id: 5, name: "Women's Bag Gloves", price: "$39.55" },
-  { id: 6, name: "Casio Digital Watch", price: "$14.99" }
+  { id: 1, name: "Nexus 6P", price: "499.99" },
+  { id: 2, name: "OnePlus 3", price: "399.99" },
+  { id: 3, name: "Kindle Paperwhite", price: "119.99" },
+  { id: 4, name: "Reversible Down Comforter", price: "45.99" },
+  { id: 5, name: "Women's Bag Gloves", price: "39.55" },
+  { id: 6, name: "Casio Digital Watch", price: "14.99" }
 ]
 
 ReactDOM.render(
@@ -111,6 +117,6 @@ ReactDOM.render(
 );
 
 ReactDOM.render(
-  <ShoppingCart items={ITEMS} />,
+  <ShoppingCart />,
   document.getElementById("shoppingCart")
 );
