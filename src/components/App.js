3d71e74a1f0c04fe46805f372a241042cec8cf49
Hunter Stevens <onebree@gmail.com>
2016-11-15 13:48:48 -0500

    Create addToCart and pass down as props

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,29 @@ import Cart from "./Cart";
 import ListOfItems from "./ListOfItems";
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.addToCart = this.addToCart.bind(this);
+
+    // getInitialState
+    this.state = {
+      items: {},
+      cart: {}
+    };
+  }
+
+  addToCart(key) {
+    // Take a copy of our state
+    const cart = {...this.state.cart};
+
+    // Update or add a new number of items ordered
+    cart[key] = cart[key] + 1 || 1;
+
+    // Update our state
+    this.setState({ cart });
+  }
+
   render() {
     return (
       <div className="container">
@@ -10,7 +33,7 @@ class App extends React.Component {
         <hr />
         <div className="row">
           <div className="col-md-8">
-            <ListOfItems />
+            <ListOfItems addToCart={this.addToCart} />
           </div>
           <div className="col-md-4">
             <Cart />
--- src/components/ListOfItems.js
+++ src/components/ListOfItems.js
@@ -8,16 +8,16 @@ class ListOfItems extends React.Component {
         <h2>Items For Sale</h2>
         <div className="row">
           <div className="col-md-4">
-            <Item key="item1" name="Nexus 6P" price={499.99}/>
+            <Item key="item1" index="item1" name="Nexus 6P" price={499.99} addToCart={this.props.addToCart} />
           </div>
           <div className="col-md-4">
-            <Item key="item2" name="OnePlus 3" price={399.99}/>
+            <Item key="item2" index="item2" name="OnePlus 3" price={399.99} addToCart={this.props.addToCart} />
           </div>
           <div className="col-md-4">
-            <Item key="item3" name="Google Pixel" price={649}/>
+            <Item key="item3" index="item3" name="Google Pixel" price={649} addToCart={this.props.addToCart} />
           </div>
           <div className="col-md-4">
-            <Item key="item4" name="Google Pixel XL" price={769}/>
+            <Item key="item4" index="item4" name="Google Pixel XL" price={769} addToCart={this.props.addToCart} />
           </div>
         </div>
       </div>
